04139fd9949d420eade474405c54d7213865eb6e
Sébastien <slopez@et.intechinfo.fr>
2023-06-26 15:05:35 +0200

    First Commit - New Branch Created

--- Capital_Avenue/Services/ConfigService.cs
+++ Capital_Avenue/Services/ConfigService.cs
@@ -13,7 +13,7 @@ namespace Capital_Avenue.Services
             return new List<string> {
                 "#6AFF2D",
                 "#401ad6",
-                "#401ad6",
+                "a98c12",
                 "#6AFF2D"
             };
         }
