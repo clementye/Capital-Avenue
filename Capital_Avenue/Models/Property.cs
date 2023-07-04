ba780e8002646f180c2ea4865628ea1860f7d2b3
oussama <arba@et.intech.fr>
2023-07-04 14:28:02 +0200

    house and hostel rent prices

--- Capital_Avenue/Models/Property.cs
+++ Capital_Avenue/Models/Property.cs
@@ -20,11 +20,12 @@ namespace Capital_Avenue.Models
         public int PriceTwoHome { get; set; }
         public int PriceThreeHome { get; set; }
         public int PriceFourHome { get; set; }
+        public int PriceHostel { get; set; }
 
 
         public Player Owner { get; set; }
       
-        public Property(int index, string name, ColorProperty groupe, Color color, int prix, int loyer, int PriceOneHome, int PriceTwoHome, int PriceThreeHome,int PriceFourHome) : base(index, name)
+        public Property(int index, string name, ColorProperty groupe, Color color, int prix, int loyer, int PriceOneHome, int PriceTwoHome, int PriceThreeHome,int PriceFourHome,int PriceHostel) : base(index, name)
         {
             this.Groupe = groupe;
             this.Name = name;
@@ -36,6 +37,7 @@ namespace Capital_Avenue.Models
             this.PriceTwoHome = PriceTwoHome;
             this.PriceThreeHome = PriceThreeHome;
             this.PriceFourHome = PriceFourHome;
+            this.PriceHostel = PriceHostel;
             housesCount = 0;
             this.Owner = null;
         }
